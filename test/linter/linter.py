56d54e5db24b0343bc0911b0f2c27cabf98e2a55
CoGn151oN <121181789+CoGn151oN@users.noreply.github.com>
2024-01-17 23:19:49 +0100

    linter ci

--- test/linter/linter.py
+++ test/linter/linter.py
@@ -1,48 +1,25 @@
 import os
-import sys
 from pylint import lint
-# from secrets import PROJECT_FOLDER
-
-print("puppyfarts")
-
-# def list_files_recursively(folder_path):
-#     file_list = []
-#     for root, dirs, files in os.walk(folder_path):
-#         for file in files:
-#             if file.endswith(".py"):
-#                 file_list.append(os.path.join(root, file))
-#     return file_list
-#
-#
-# files = list_files_recursively(PROJECT_FOLDER)
-#
-# for f in files:
-#     THRESHOLD = 10
-#
-#     run = lint.Run([f], exit=False)
-#     score = run.linter.stats.global_note
-#
-#     print(f"Score: {score}")
 
 
+def list_files_recursively(folder_path):
+    file_list = []
+    for root, dirs, files in os.walk(folder_path):
+        dirs[:] = [d for d in dirs if "venv" not in d]
+        for file in files:
+            if file.endswith(".py"):
+                file_list.append(os.path.join(root, file))
+    return file_list
 
 
+if __name__ == '__main__':
+    path = os.path.dirname(os.path.dirname(os.getcwd()))
+    files = list_files_recursively(path)
 
-    # if score < THRESHOLD:
-    #     print(f"Linter failed, Score: {score} < threshold: {THRESHOLD}")
-    #     sys.exit(1)
-    # sys.exit(0)
+    for f in files:
+        THRESHOLD = 10
 
-# THRESHOLD = 10
+        run = lint.Run([f], exit=False)
+        score = run.linter.stats.global_note
 
-# run = lint.Run(["../main.py"], exit=False)
-# score = run.linter.stats.global_note
-#
-# print(f"Score: {score}")
-#
-# if score < THRESHOLD:
-#     print(f"Linter failed, Score: {score} < threshold: {THRESHOLD}")
-#     sys.exit(1)
-# sys.exit(0)
-
-
+        print(f"Score: {score}")
