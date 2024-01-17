0244163450db6b3bc6f84047ee137511dd9349e3
CoGn151oN <121181789+CoGn151oN@users.noreply.github.com>
2024-01-17 16:44:29 +0100

    linter test

--- test/linter/linter.py
+++ test/linter/linter.py
@@ -3,25 +3,30 @@ import sys
 from pylint import lint
 from secrets import PROJECT_FOLDER
 
+print("puppyfarts")
 
-def list_files_recursively(folder_path):
-    file_list = []
-    for root, dirs, files in os.walk(folder_path):
-        for file in files:
-            if file.endswith(".py"):
-                file_list.append(os.path.join(root, file))
-    return file_list
+# def list_files_recursively(folder_path):
+#     file_list = []
+#     for root, dirs, files in os.walk(folder_path):
+#         for file in files:
+#             if file.endswith(".py"):
+#                 file_list.append(os.path.join(root, file))
+#     return file_list
+#
+#
+# files = list_files_recursively(PROJECT_FOLDER)
+#
+# for f in files:
+#     THRESHOLD = 10
+#
+#     run = lint.Run([f], exit=False)
+#     score = run.linter.stats.global_note
+#
+#     print(f"Score: {score}")
 
 
-files = list_files_recursively(PROJECT_FOLDER)
 
-for f in files:
-    THRESHOLD = 10
 
-    run = lint.Run([f], exit=False)
-    score = run.linter.stats.global_note
-
-    print(f"Score: {score}")
 
     # if score < THRESHOLD:
     #     print(f"Linter failed, Score: {score} < threshold: {THRESHOLD}")
