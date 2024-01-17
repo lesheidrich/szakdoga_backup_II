6b0556f5953a9dbb2b34ccd9361a8602a66ff26e
CoGn151oN <121181789+CoGn151oN@users.noreply.github.com>
2024-01-17 21:12:53 +0100

    regression test

---
+++ test/regression.py
@@ -0,0 +1,17 @@
+import unittest
+from test.unit.test_logger import TestLogger
+from test.unit.test_proxy_handler import TestProxyHandler
+
+
+def suite():
+    test_suite = unittest.TestSuite()
+
+    test_suite.addTest(unittest.TestLoader().loadTestsFromTestCase(TestLogger))
+    test_suite.addTest(unittest.TestLoader().loadTestsFromTestCase(TestProxyHandler))
+
+    return test_suite
+
+
+if __name__ == '__main__':
+    runner = unittest.TextTestRunner()
+    runner.run(suite())
